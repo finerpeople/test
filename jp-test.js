151a42a607d7390a871b165a70e0b7b53f8103ea
James Page <jcpage02@outlook.com>
2019-02-11 09:38:16 -0700

    first commit

---
+++ jp-test.js
@@ -0,0 +1,32 @@
+const fun = (event) => {
+    console.log(event)
+}
+
+
+
+
+
+
+
+// create a new branch: 
+// 	git checkout -b <name of the branch>
+// switch branches: 
+// 	git checkout <name of the branch>
+// what branch am I on? 
+// 	git branch
+
+
+// To start coding, create a new branch. When you are done with whatever feature you are building, and you want your code merged into the master branch, do the following:
+
+// 1. git add .
+// 2. git commit -m 'your commit message'
+// 3. git checkout master
+// 4. git pull origin master
+// 5. git checkout <branch name>
+// 6. git merge master
+// 	a. if there are merge conflicts, fix them
+// 	b. git add .
+// 	c. git commit -m 'fix merge conflicts'
+// 7. git push origin <name of the branch>
+
+// Then go to github and submit a pull request. Notify your team lead.
